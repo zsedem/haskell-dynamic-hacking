adb680c72b6d099a26a8264c7d621c0bfe127e1f
Zsigmond Adam Oliver <zsedem@gmail.com>
2015-03-14 22:32:42 +0000

    Working dynamic loading of a typeclass implementation in runtime
    I mean:
    We have a value in the ForDynamiclyLoad module which can be any value
    which is instance of the Show TypeClass and in the Main I can easily
    use the show method of this class.
    
    This will be useful for creating some dynamic loading of some complex
    Functionality which implements a simple API defined by a TypeClass
    
    Signed-off-by: Zsigmond Adam Oliver <zsedem@gmail.com>

--- Main.hs
+++ Main.hs
@@ -1,10 +1,11 @@
-import System.Plugins.Load(load_, LoadStatus(..))
+import System.Plugins.Load(load, LoadStatus(..))
+import Core
 
 main :: IO ()
 main = do
-    module_result <- load_ "ForDynamiclyLoad.o" [] "value"
+    module_result <- load "ForDynamiclyLoad.o" ["."] [] "value"
     case module_result of
         LoadFailure msg -> do
             print msg
-        LoadSuccess _ (v::String) -> do
+        LoadSuccess _ (v::Showable) -> do
             print v
