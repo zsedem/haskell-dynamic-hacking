a2b253c6e6cdf4d1db7313d22184a38499c3c2ac
Zsigmond Adam Oliver <zsedem@gmail.com>
2015-03-15 23:12:00 +0000

    foo: bar
    Signed-off-by: Zsigmond Adam Oliver <zsedem@gmail.com>

--- Core.hs
+++ Core.hs
@@ -1,7 +1,23 @@
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE ExistentialQuantification,
+             DeriveDataTypeable,
+             OverlappingInstances #-}
 module Core where
+import Data.Typeable
+import Data.Dynamic
+import Data.Maybe
+import Data.Data
 
 data Showable = forall a. Show a => Showable a
+    deriving(Typeable)
+
+
+retrive = let str = (show $ typeOf retrive)
+          in  fromJust $ fromDynamic $ fromJust $ lookup str simpleData
+
+simpleData :: [(String, Dynamic)]
+simpleData = zip (map (show.dynTypeRep) sdata') sdata'
+  where
+    sdata' = [toDyn "5", toDyn $ Showable "4", toDyn "5"]
 
 instance Show Showable where
   show (Showable x) = show x
